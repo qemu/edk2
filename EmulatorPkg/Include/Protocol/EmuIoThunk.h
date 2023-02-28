25269d6076497355c7d5df8f50a6e2ade921f1b4
Nate DeSimone <nathaniel.l.desimone@intel.com>
2023-02-28 14:55:01 -0800

    EmulatorPkg: Delete EMU_IO_THUNK_PROTOCO_GUID
    Delete this macro because:
    
    1. Nothing uses it.
    2. It is misspelled.
    3. It is a EDK I style GUID definition which is redundant with
       an existing EDK II style GUID definition.
    
    Cc: Andrew Fish <afish@apple.com>
    Cc: Ray Ni <ray.ni@intel.com>
    Cc: Michael D Kinney <michael.d.kinney@intel.com>
    Cc: Chasel Chiu <chasel.chiu@intel.com>
    Signed-off-by: Nate DeSimone <nathaniel.l.desimone@intel.com>

--- EmulatorPkg/Include/Protocol/EmuIoThunk.h
+++ EmulatorPkg/Include/Protocol/EmuIoThunk.h
@@ -2,6 +2,7 @@
   Emulator Thunk to abstract OS services from pure EFI code
 
   Copyright (c) 2010 - 2011, Apple Inc. All rights reserved.<BR>
+  Copyright (c) 2023, Intel Corporation. All rights reserved.<BR>
 
   SPDX-License-Identifier: BSD-2-Clause-Patent
 
@@ -9,9 +10,6 @@
 
 #ifndef __EMU_IO_THUNK__
 #define __EMU_IO_THUNK__
-
-#define EMU_IO_THUNK_PROTOCO_GUID  \
- { 0x453368F6, 0x7C85, 0x434A, { 0xA9, 0x8A, 0x72, 0xD1, 0xB7, 0xFF, 0xA9, 0x26 } }
 
 typedef struct _EMU_IO_THUNK_PROTOCOL EMU_IO_THUNK_PROTOCOL;
 
