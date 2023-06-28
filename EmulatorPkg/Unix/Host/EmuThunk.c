a7e5e433d576e841f39e8d22c6ee08ce27d310ec
Chasel Chiu <chasel.chiu@intel.com>
2023-06-28 11:28:25 -0700

    EmulatorPkg/Host: Add QueryPerformanceCounter for ARM Linux.
    For performance measurement support, implemented
    QueryPerformanceCounter and verified by RaspberryPi platform.
    
    Change-Id: I767d5dd8f26b55adcb659b764527ef57b285ad9c
    Hsd-es-id: <HSD-ES bug/ise id>
    Signed-off-by: Chasel Chiu <chasel.chiu@intel.com>

--- EmulatorPkg/Unix/Host/EmuThunk.c
+++ EmulatorPkg/Unix/Host/EmuThunk.c
@@ -272,8 +272,13 @@ QueryPerformanceCounter (
 
   return (Start * sTimebaseInfo.numer) / sTimebaseInfo.denom;
  #else
-  // Need to figure out what to do for Linux?
-  return 0;
+  //
+  // Below is verified by RaspberryPi Linux.
+  //
+  struct timespec  ts1;
+
+  clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &ts1);
+  return (ts1.tv_nsec + (ts1.tv_sec * 1000000000)); #endif
  #endif
 }
 
